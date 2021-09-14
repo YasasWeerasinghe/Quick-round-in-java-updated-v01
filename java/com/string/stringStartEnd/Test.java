53e19d69517a55995fa78148c0648f098fbf3b8f
Yasas <yasas.2015041@iit.ac.lk>
2021-09-14 17:45:10 +0530

    Strings

---
+++ java/com/string/stringStartEnd/Test.java
@@ -0,0 +1,35 @@
+package com.string.stringStartEnd;
+
+public class Test {
+
+    public static void main(String args[]) {
+
+        String[] strings = {"started", "starting", "ended", "ending"};
+        // test method startsWith
+        for (String string : strings) {
+            if (string.startsWith("st")) {
+                System.out.printf("\"%s\" starts with \"st\"%n", string);
+            }
+        }
+
+        System.out.println();
+
+        // test method startsWith starting from position 2 of string
+        for (String string : strings) {
+            if (string.startsWith("art", 2)) {
+                System.out.printf(
+                        "\"%s\" starts with \"art\" at position 2%n", string);
+            }
+        }
+
+        System.out.println();
+
+        // test method endsWith
+        for (String string : strings) {
+            if (string.endsWith("ed")) {
+                System.out.printf("\"%s\" ends with \"ed\"%n", string);
+            }
+        }
+
+    }
+}
---
+++ java/com/string/subString/Test.java
@@ -0,0 +1,17 @@
+package com.string.subString;
+
+public class Test {
+
+    public static void main(String args[]) {
+
+        String letters = "abcdefghijklmabcdefghijklm";
+
+        // test substring methods
+        System.out.printf("Substring from index 20 to end is \"%s\"%n",
+                letters.substring(20));
+        System.out.printf("%s \"%s\"%n",
+                "Substring from index 3 up to, but not including 6 is",
+                letters.substring(3, 6));
+
+    }
+}
